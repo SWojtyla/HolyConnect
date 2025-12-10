37fa7c3b70ec16062a120f069fd20c7ce147a39a
Sebastien Wojtyla <sebastien.wojtyla@portima.onmicrosoft.com>
2025-12-10 13:28:48 +0100

    Improve vars UI

---
+++ src/HolyConnect.Maui/Components/Shared/VariableModel.cs
@@ -0,0 +1,8 @@
+namespace HolyConnect.Maui.Components.Shared;
+
+public class VariableModel
+{
+    public string Key { get; set; } = string.Empty;
+    public string Value { get; set; } = string.Empty;
+    public bool IsSecret { get; set; } = false;
+}
